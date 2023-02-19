106f687af413ca53768d4cf6b3b167b9fefde3cc
IrinaKolganova <irene1805@yandex.ru>
2023-02-19 04:09:26 +0300

    updated star files

--- star.js
+++ star.js
@@ -1,8 +1,8 @@
-function isLeapYear(year){
-    let message=''
-    if(year/4){message='Год является високосным (366 дней)'}
-    else{}
-    return message
+let year = prompt('Напишите, какой год вас интересует?');
+if (year%4 == 0) {
+    if (year%100 == 0) {
+        if (year%400 == 0) {alert( 'Год является високосным (366 дней)' );} else{alert( 'Год не високосный (365 дней)' );}
+    } else{alert( 'Год является високосным (366 дней)' );}
+  } else {
+  alert( 'Год не високосный (365 дней)' ); 
 }
-
-console.log(isLeapYear(365))
